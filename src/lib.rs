d45cb9c47617909e113c0c101874940572809ed4
Nelson Penn <nelsonapenn@protonmail.com>
2024-11-09 23:43:03 -0500

    Start to add error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![cfg_attr(not(test), no_std)]
 use core::arch::asm;
+pub mod error;
+pub mod i2c;
 pub mod syscall_number;
+
+use error::Error;
 
 pub type FileDescriptor = i32;
 
@@ -11,57 +15,73 @@ pub const STDERR: FileDescriptor = 2;
 const AT_FDCWD: i32 = -100;
 
 #[repr(i32)]
-pub enum OpenFlags
-{
+pub enum OpenFlags {
     ReadOnly = 0,
     WriteOnly = 1,
     ReadWrite = 2,
 }
 
-
-
-pub fn open(path: &str, flags: OpenFlags) -> FileDescriptor
-{
+pub fn open(path: &str, flags: OpenFlags) -> Result<FileDescriptor, Error> {
     let fd;
     let ptr: *const u8 = path.as_ptr();
-    unsafe {asm!(
-        "svc 0",
-        inout("x0") 0 /* dir fd, if not absolute */ => fd,
-        in("x1") ptr,
-        in("x2") flags as i32,
-        in("x3") 0, /* mode */
-    )};
-    fd
+    unsafe {
+        asm!(
+            "svc 0",
+            inout("x0") 0 /* dir fd, if not absolute */ => fd,
+            in("x1") ptr,
+            in("x2") flags as i32,
+            in("x3") 0, /* mode */
+            in("w8") syscall_number::OPEN_AT
+        )
+    };
+    if fd > 0 {
+        Ok(fd)
+    } else {
+        Err((-fd).into())
+    }
 }
 
-pub fn write(fd: FileDescriptor, bytes: &[u8]) -> i32
-{
+pub fn close(fd: FileDescriptor) -> i32 {
+    let retval;
+    unsafe {
+        asm!(
+            "svc 0",
+            inout("x0") fd => retval,
+            in("w8") syscall_number::CLOSE,
+        )
+    };
+    retval
+}
+
+pub fn write(fd: FileDescriptor, bytes: &[u8]) -> i32 {
     let bytes_written;
     let ptr: *const u8 = bytes.as_ptr();
-    unsafe {asm!(
-        "svc 0",
-        inout("w0") fd => bytes_written,
-        in("x1") ptr,
-        in("x2") bytes.len(),
-        in("w8") syscall_number::WRITE
-    )};
+    unsafe {
+        asm!(
+            "svc 0",
+            inout("w0") fd => bytes_written,
+            in("x1") ptr,
+            in("x2") bytes.len(),
+            in("w8") syscall_number::WRITE
+        )
+    };
     bytes_written
 }
 
-pub fn read(fd: FileDescriptor, bytes: &mut [u8]) -> i32
-{
+pub fn read(fd: FileDescriptor, bytes: &mut [u8]) -> i32 {
     let bytes_read;
     let ptr: *mut u8 = bytes.as_mut_ptr();
-    unsafe {asm!(
-        "svc 0",
-        inout("w0") fd => bytes_read,
-        in("x1") ptr,
-        in("x2") bytes.len(),
-        in("w8") syscall_number::READ
-    )};
+    unsafe {
+        asm!(
+            "svc 0",
+            inout("w0") fd => bytes_read,
+            in("x1") ptr,
+            in("x2") bytes.len(),
+            in("w8") syscall_number::READ
+        )
+    };
     bytes_read
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -69,11 +89,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        // let fd = open("/dev/i2c-1", OpenFlags::ReadWrite);
-        // println!("{}", fd);
-        let mut buf = [0; 8];
-        let count = read(STDIN, &mut buf);
-        let outbuf = format!("count: {}, read: {}", count, std::str::from_utf8(&buf).unwrap());
-        write(STDOUT, outbuf.as_bytes());
+        let fd = open("/dev/i2c-1\0", OpenFlags::ReadWrite);
+        println!("{}", fd);
+        let ret = close(fd);
+        println!("ret {}", ret);
     }
 }
